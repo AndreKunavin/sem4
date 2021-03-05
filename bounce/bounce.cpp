346c517227ec9bc5294d77e5e719e4c454065314
Andrei Kunavin <kunavin.aa@phystech.edu>
2021-03-06 00:14:41 +0300

    added new type of ball (push right bottom of the mouse)

--- bounce/bounce.cpp
+++ bounce/bounce.cpp
@@ -6,19 +6,23 @@
 #define X 1000
 #define Y 800
 #define G 400               // accelleration
-#define R 20.f              // radius
-#define N 5                 // number of balls added by one click  
-#define M 100.f             // mass
+#define R1 50.f
+#define M1 500.f
+#define R2 20.f              // radius 2
+#define N 6                 // number of small balls (2) added by one click  
+#define M2 100.f             // mass 2
 #define LOSSES_FACTOR 0.6   // 0 <= LOSSES_FACTOR <= 1 
 
 struct Particle {
     Particle() {};
-    Particle(Vector2 pos_, Vector2 vel_, float radius, float mass) {
+    Particle(Vector2 pos_, Vector2 vel_, float radius, float mass, std::size_t type) {
         pos = pos_;
         vel = vel_;
         r = radius;
         m = mass;
+        t = type;
     }
+    std::size_t t;
     Vector2 pos;
     Vector2 vel;
     float r;
@@ -76,31 +80,59 @@ public:
     }
 };
 
-sf::Color color_and_velocity(float vel_abs) {
+sf::Color color_and_velocity(float vel_abs, std::size_t type) {
     sf::Uint16 cf = vel_abs / 2; 
     sf::Color color; 
     if (cf > 255) {
         cf = 255;
     }
-    color.g = cf;
-    color.r = (color.g + 255) / 2;
-    color.b = 0;
+    if (type == 1) {
+        color.g = cf;
+        color.r = (color.g + 255) / 2;
+        color.b = 0;
+    } else {
+        color.g = 0;
+        color.r = cf;
+        color.b = 128; 
+    }
     return color;
+}
+
+void change_type(std::size_t& type) {
+    if (type == 0) {
+        type = 1;
+    } else {
+        type = 0;
+    }
 }
 
 class Mouse {
 public:
-    Mouse(float radius, float mass) {
-        ball.r = radius;
-        ball.m = mass;
-        phantom.setRadius(radius);
+    Mouse(float radius1, float mass1, float radius2, float mass2) {
+        first.r = radius1;
+        first.m = mass1;
+        first.t = 0;
+        second.r = radius2;
+        second.m = mass2;
+        second.t = 1;
     };
     sf::CircleShape phantom;
     sf::Vector2f particle_pos;
     sf::Vector2f cursor_pos;
+    std::size_t ball_type = 1;
 
     void tracking(World& W, sf::RenderWindow& window) {
         sf::Vector2f e(1.f, 1.f);
+        ball.push_back(first);
+        ball.push_back(second);
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Right)) {
+            if (mode_r == 0) {
+                change_type(ball_type);
+                mode_r = 1;
+            }
+        } else {
+            mode_r = 0;
+        }
         if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
             sf::Vector2i m_particle_pos;
             sf::Vector2i m_cursor_pos;
@@ -118,30 +150,31 @@ public:
                 };
                 window.draw(line, 2, sf::Lines);
             }
-            phantom.setPosition(particle_pos - e * ball.r);
-            ball.vel.x = 3.f * (cursor_pos.x - particle_pos.x);
-            ball.vel.y = 3.f * (cursor_pos.y - particle_pos.y);
-            
-            sf::Color color = color_and_velocity(ball.vel.len());
+            phantom.setRadius(ball[ball_type].r);
+            phantom.setPosition(particle_pos - e * ball[ball_type].r);
+            ball[ball_type].vel.x = 3.f * (cursor_pos.x - particle_pos.x);
+            ball[ball_type].vel.y = 3.f * (cursor_pos.y - particle_pos.y);
+            sf::Color color = color_and_velocity(ball[ball_type].vel.len(), ball_type);
             phantom.setFillColor(color);
-            
             window.draw(phantom);
         } else {
             if (mode == 1) {
-                ball.pos.x = particle_pos.x - ball.r;
-                ball.pos.y = particle_pos.y - ball.r;            
-                for(size_t i = 0; i < N; i++) {
-                    W.add(ball);
-                }
-                    
+                ball[ball_type].pos.x = particle_pos.x - ball[ball_type].r;
+                ball[ball_type].pos.y = particle_pos.y - ball[ball_type].r;            
+                for(size_t i = 0; i <= ball_type * (N - 1); i++) {
+                    W.add(ball[ball_type]);
+                }     
                 mode = 0;
             }
         }
         
     }
 private:
+    std::size_t mode_r = 0;
     std::size_t mode = 0;
-    Particle ball;
+    Particle first;
+    Particle second;
+    std::vector <Particle> ball;
 };
 
 void draw(World& W, sf::RenderWindow& window) {
@@ -153,7 +186,7 @@ void draw(World& W, sf::RenderWindow& window) {
         balls[i].setPosition(new_pos[i]);
         balls[i].setRadius(W.particles[i].r);
 
-        sf::Color color = color_and_velocity(W.particles[i].vel.len());
+        sf::Color color = color_and_velocity(W.particles[i].vel.len(), W.particles[i].t);
         balls[i].setFillColor(color);
 
         window.draw(balls[i]);
@@ -173,7 +206,7 @@ int main() {
     sprite.setTexture(texture);
 
     World W;
-    Mouse mouse(R, M);
+    Mouse mouse(R1, M1, R2, M2);
 
     sf::Clock clock;
     sf::Time prev_time;
